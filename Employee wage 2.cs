efa8667ef4c23abace9335cdde7dda5ff250e3ef
“reachsumit1” <harry.pappi143@gmail.com>
2023-05-22 22:39:55 +0530

    Employeewage

---
+++ Employee wage 2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+
+        int dailyWage = wagePerHour * fullDayHours;
+
+        Console.WriteLine("Daily Employee Wage: $" + dailyWage);
+    }
+}
