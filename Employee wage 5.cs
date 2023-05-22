237aa7d31efe5425f903c4c40fd9573ba0379458
“reachsumit1” <harry.pappi143@gmail.com>
2023-05-22 22:57:45 +0530

    Employee wage 5.cs

---
+++ Employee wage 5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        const int WorkingDaysPerMonth = 20;
+        const double DailyWage = 100.0;
+
+        double monthlyWage = WorkingDaysPerMonth * DailyWage;
+
+        Console.WriteLine("Wages for a month (assuming 20 working days): $" + monthlyWage);
+    }
+}
