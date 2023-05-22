a6778ca9fe8a543c2d9793355b3dca654c436ca3
Harishkumar Yetam <119168445+harrypappi@users.noreply.github.com>
2023-05-22 23:15:02 +0530

    Add files via upload

---
+++ Employee wage 6.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        const int MaxWorkingHours = 100;
+        const int MaxWorkingDays = 20;
+        const double HourlyRate = 10.0;
+
+        int totalWorkingHours = 0;
+        int totalWorkingDays = 0;
+        double totalWages = 0.0;
+
+        while (totalWorkingHours < MaxWorkingHours && totalWorkingDays < MaxWorkingDays)
+        {
+            totalWorkingHours += 8;
+            totalWorkingDays++;
+            totalWages += (8 * HourlyRate);
+        }
+
+        Console.WriteLine("Total working hours: " + totalWorkingHours);
+        Console.WriteLine("Total working days: " + totalWorkingDays);
+        Console.WriteLine("Total wages: $" + totalWages);
+    }
+}
